f0d8bc41003f675d01895f822ce201c43d86edba
wsxingjun <1185889308@qq.com>
2018-01-09 09:43:41 +0800

    v2-1.完成item中的内容复制到EditText框中，2.完成delete按钮的删除功能

--- app/src/main/java/it/oztaking/com/droplistview/MainActivity.java
+++ app/src/main/java/it/oztaking/com/droplistview/MainActivity.java
@@ -4,22 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemClickListener{
 
     private EditText et_input;
     private ListView listView;
     private ArrayList<String> datas;
+    private PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +46,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void showPopupWindow() {
         initListView();
         //显示下拉选择框
-        PopupWindow popupWindow = new PopupWindow(listView, et_input.getWidth(), 300);
+        popupWindow = new PopupWindow(listView, et_input.getWidth(), 300);
+        //设置可以获取焦点
+        popupWindow.setFocusable(true);
         //显示在指定的控件下
         popupWindow.showAsDropDown(et_input,0,-5);
     }
@@ -54,6 +59,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //去掉分割线
         listView.setDividerHeight(0);
         listView.setBackgroundResource(R.drawable.listview_background);
+        //增加监听事件
+        listView.setOnItemClickListener(this);
         datas = new ArrayList<String>();
         //创建一些数据
         for (int i=0; i<30; i++){
@@ -62,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listView.setAdapter(new MyAdapter());
 
     }
+
 
 
     private class MyAdapter extends BaseAdapter {
@@ -81,7 +89,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             View view;
             //view 复用
             if (convertView ==  null){
@@ -93,7 +101,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //设置内容
             TextView tv_number_qq = (TextView) view.findViewById(R.id.tv_number_qq);
             tv_number_qq.setText(datas.get(position));
+
+            //响应删除事件
+            view.findViewById(R.id.ib_delete).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    datas.remove(position);//将选中的条目移除
+                    notifyDataSetChanged(); //移除之后需要更细界面
+                    if (datas.size() == 0){
+                        popupWindow.dismiss();
+                    }
+                }
+            });
             return view;
         }
     }
+
+    //条目的点击事件
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(),"positon:"+position,Toast.LENGTH_SHORT).show();
+        String string = datas.get(position);
+        et_input.setText(string);
+        popupWindow.dismiss();
+
+    }
+
 }
